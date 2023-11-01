d0d63adaa15fe424da2a9aaa2ac452b25ae2a3d8
wnf <william@fluxrme.com>
2023-10-31 19:30:16 -0500

    adds delete user functionality - now working on cleanup to reach MVP

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const User = require('../models/user')
 
 module.exports = {
   index, 
-  show
+  show, 
+  requestDeleteUser: requestDeleteUser,
+  confirmDeleteUser: confirmDeleteUser,
 }
 
 function index(req, res) {
@@ -25,4 +27,24 @@ async function show(req, res) {
       userName: userName,
     }
   );
+}
+
+function requestDeleteUser(req, res) {
+  // console.log('REQUEST DELETE USER FUNCTION CALLED')
+  res.render(
+    'delete-user',
+    {userId: req.user.id}
+  )
+}
+
+async function confirmDeleteUser(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+    console.log(user)
+    await User.deleteOne({ _id: req.user.id })
+    res.redirect('/logout')
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).send('Error deleting user.');
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,29 @@ const passport = require('passport');
 const indexController = require("../controllers/index.js")
 
 router.get('/', indexController.index);
+
 router.get('/profile', indexController.show);
 
-// Google OAuth login route
+router.get('/delete-user', indexController.requestDeleteUser)
+
+router.post('/:id', indexController.confirmDeleteUser)
+
 router.get('/auth/google', passport.authenticate(
   'google',
   {
     scope: ['profile', 'email'], // Requesting the user's profile and email
   }
-));
+)); // Google OAuth login route
 
-// Google OAuth callback route
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
     successRedirect: '/profile',
     failureRedirect: '/'
   }
-));
+)); // Google OAuth callback route
 
-// OAuth logout route
-router.get('/logout', function(req, res){
+router.get('/logout', function(req, res){ // OAuth logout route
   req.logout(function() {
     res.redirect('/');
   });
